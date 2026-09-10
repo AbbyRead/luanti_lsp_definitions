176fb97d787f6692633445be4365b0417d23d28c
Abigail Read <AbbyReadMail@gmail.com>
2026-09-09 20:19:13 -0600

    Add a retrun value to EntityDef's get_staticdata function

--- entity.lua
+++ entity.lua
@@ -1149,5 +1149,5 @@
 ---@field on_attach_child? fun(self, child:ObjectRef)
 ---@field on_detach_child? fun(self, child:ObjectRef)
 ---@field on_detach? fun(self, parent:ObjectRef)
----@field get_staticdata? fun(self)
+---@field get_staticdata? fun(self):string
 ---@field initial_properties ObjectProps
